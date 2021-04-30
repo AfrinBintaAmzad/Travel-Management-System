e0c27ebe7404f3044373fc73de4e49a2cf9ffa4e
AfrinBintaAmzad <83439797+AfrinBintaAmzad@users.noreply.github.com>
2021-04-30 20:02:14 +0600

    Add files via upload

---
+++ PlanBuilder.java
@@ -0,0 +1,29 @@
+package com.afrin.controller;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.afrin.entity.Plan;
+
+public class PlanBuilder {
+
+	private Plan plan;
+	
+	public PlanBuilder(){
+		plan = new Plan();
+	}
+	
+	public PlanBuilder userId(int id){
+		ReflectionTestUtils.setField(plan, "userId", id);
+		return this;
+	}
+	
+	public PlanBuilder place(String place){
+		ReflectionTestUtils.setField(plan, "place", place);
+		return this;
+	}
+	
+	public Plan build(){
+		return plan;
+	}
+
+}
